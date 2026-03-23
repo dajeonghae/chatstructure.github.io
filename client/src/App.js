00859dfe04a5641513e5fb964c127e5b1b3e3141
dajeonghae <happyjde3@gmail.com>
2026-03-24 08:56:32 +0900

    feat: login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux"; 
-import { store } from "./redux/store"; 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import Main from "./pages/main";
+import Login from "./pages/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <Login onLogin={setUser} />;
+  }
+
   return (
     <Provider store={store}>
       <BrowserRouter basename="/chatstructure">
